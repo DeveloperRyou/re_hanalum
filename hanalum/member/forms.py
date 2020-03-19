1bfa7c45245fc7a633bcf79a632ae0883c6b1ad9
happykys <42750885+happykys@users.noreply.github.com>
2020-03-19 14:13:07 +0900

    비밀번호 8자리 정규식 적용

--- hanalum/member/forms.py
+++ hanalum/member/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from .models import User
+import re
 
 
 class UserCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label='비밀번호', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '영문 + 숫자로 8자 이상'}))
-    password2 = forms.CharField(label='비밀번호 재확인', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '영문 + 숫자로 8자 이상'}))
+    password1 = forms.CharField(label='비밀번호', widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': '영문 + 숫자로 8자 이상'}))
+    password2 = forms.CharField(label='비밀번호 재확인', widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': '영문 + 숫자로 8자 이상'}))
 
     class Meta:
         model = User
@@ -25,12 +28,24 @@ class UserCreationForm(forms.ModelForm):
             'admission_year': '입학년도',
         }
 
-    def check_password2(self):
+    def check_password(self): # 비밀번호 확인
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
+
+        p = re.compile("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")
+        is_match = p.match(password1)  # 여기에 비밀번호 정규식
+        if is_match is None:  # 비밀번호가 정규식에 매치됨
+            return "비밀번호는 8자리 이상 소문자+숫자"
+
         if password1 and password2 and password1 != password2:
             return "비밀번호가 일치하지 않습니다."
         return None
+
+    def check_realname(self): # 실명확인
+        return
+
+    def check_email(self, _email):
+        return User.objects.filter(email=_email).count()
 
     def save(self, commit=True):
         # 비밀번호를 해시 상태로 저장
@@ -39,6 +54,7 @@ class UserCreationForm(forms.ModelForm):
         if commit:
             user.save()
         return user
+
 
 class UserChangeForm(forms.ModelForm):
     password = ReadOnlyPasswordHashField(label='password')
--- hanalum/member/views.py
+++ hanalum/member/views.py
@@ -7,8 +7,13 @@ from django.shortcuts import redirect
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
+        if form.check_email(request.POST['email']) > 0: # 이미 있는 이메일
+            return render(request, 'register.html', {'form': form, 'error': "email error"})
+        else: # 사용 가능한 이메일
+            return redirect('login')
+
         if form.is_valid():
-            is_error = form.check_password2()
+            is_error = form.check_password()
             if is_error is None:
                 form.save()
                 return redirect('login')
