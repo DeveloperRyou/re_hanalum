7a7734c5779dcdcf2a80453db76d0f31580a6639
kes02 <hijin1220@gmail.com>
2020-03-30 15:17:00 +0900

    main.models 랑 admin.py 변경

--- hanalum/hanalum/settings.py
+++ hanalum/hanalum/settings.py
@@ -145,8 +145,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Media files - 업로드를 하는 URL과 디렉토리 설정
+MEDIA_URL = '/files/' #업로드할 경로
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads') #로컬 디렉토리 어디에 저장할 것인지
 
 #파일 업로드 최대 용량
 #FILE_UPLOAD_MAX_MEMORY_SIZE = '???'
--- hanalum/main/models.py
+++ hanalum/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from hanalum import settings
 
-# Create your models here.
+class Sidebar(models.Model):
+    menu_name = models.CharField( #메뉴이름
+        verbose_name='Menu_name',
+        max_length=20,
+    )
+    menu_url = models.URLField( #메뉴링크
+        'url',
+        blank=True,
+        null=True,
+    )
+    page_authority = models.IntegerField( #접근권한
+        verbose_name='page_authority',
+        default=0,
+    )
+    unit = models.BooleanField( #대단원소단원
+        verbose_name='unit',
+    )
+
+
