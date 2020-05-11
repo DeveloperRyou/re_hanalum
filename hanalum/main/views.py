c90146da225ad48d3d7fd43b1da43bae3ba6264f
ryou737373 <ryou737373@github.com>
2020-05-12 00:52:58 +0900

    widget weather 백엔드개발

--- hanalum/main/views.py
+++ hanalum/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from datetime import datetime, timedelta
 from .youtube import youtube_parser
+from .weather import weather_parser
 from article.models import Article
 from board.models import Board
 # Create your views here.
@@ -18,6 +19,8 @@ def main(request):
     board_free_type = Board.objects.get(board_id='free')
     board_free = Article.objects.filter(board_type=board_free_type, created_at__range=[datetime_past, datetime_now])
     board_free = board_free.order_by('like_user_set')[:ARTICLE_NUM]
+    # 날씨 크롤링
+    weather = weather_parser()
     # 유튜브 크롤링
     youtube_list = youtube_parser()
-    return render(request, 'main.html', {'board_petition':board_petition, 'board_free':board_free, 'youtube_list':youtube_list})
+    return render(request, 'main.html', {'board_petition':board_petition, 'board_free':board_free, 'weather':weather, 'youtube_list':youtube_list})
