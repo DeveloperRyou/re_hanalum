02ec5e45198870a6850556039f4ea1ade78d323f
shinkeonkim <singun11@naver.com>
2020-09-12 02:01:44 +0900

    feat: nav 보드 우선순위에 따라 정렬

--- hanalum/main/views.py
+++ hanalum/main/views.py
@@ -14,10 +14,6 @@ ARTICLE_NUM = 3
 def main(request):
     datetime_now = datetime.now(tz=timezone.utc)
     datetime_past = datetime_now + timedelta(days=-7)
-    # 청원게시판
-    board_petition_type = Board.objects.get(board_id='petition')
-    board_petition = Article.objects.filter(board_type=board_petition_type, created_at__range=[datetime_past, datetime_now])
-    board_petition = board_petition.order_by('like_user_set')[:ARTICLE_NUM]
     # 자유게시판
     board_free_type = Board.objects.get(board_id='free')
     board_free = Article.objects.filter(board_type=board_free_type, created_at__range=[datetime_past, datetime_now])
@@ -29,7 +25,7 @@ def main(request):
     # 유튜브 크롤링, 3개의 유튜브객체가 들어있는 리스트 반환
     youtube_list = youtube_parser()
 
-    category = Board.objects.all()
+    category = Board.objects.all().order_by('-priority')
 
 
-    return render(request, 'main.html', {'category': category,'board_petition': board_petition, 'board_free': board_free, 'cafeteria_list': cafeteria_list, 'youtube_list': youtube_list})
+    return render(request, 'main.html', {'category': category,'board_free': board_free, 'cafeteria_list': cafeteria_list, 'youtube_list': youtube_list})
